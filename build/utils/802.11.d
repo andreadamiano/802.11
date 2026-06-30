3f10dde2433b121191180daa711b8527430dfa50
Andrea Damiano <decong.ad@gmail.com>
2026-06-30 18:11:29 +0200

    retrieve mac address

--- build/utils/802.11.d
+++
@@ -1,6 +0,0 @@
-build/utils/802.11.o: 802.11/utils/802.11.c 802.11/utils/frames.h \
- 802.11/utils/settings.h 802.11/utils/802.11.h 802.11/utils/rawsocket.h
-802.11/utils/frames.h:
-802.11/utils/settings.h:
-802.11/utils/802.11.h:
-802.11/utils/rawsocket.h:
--- build/utils/frame_queue.d
+++
@@ -1,5 +0,0 @@
-build/utils/frame_queue.o: 802.11/utils/frame_queue.c \
- 802.11/utils/frame_queue.h 802.11/utils/frames.h 802.11/utils/settings.h
-802.11/utils/frame_queue.h:
-802.11/utils/frames.h:
-802.11/utils/settings.h:
